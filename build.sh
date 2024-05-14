a11e740e943749b42df7190378101357c4d299a1
Miki Shibabaw <119623992+amexyegondar@users.noreply.github.com>
2024-05-14 12:00:21 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-pip install --upgrade pip
-pip install --no-cache-dir -r requirements.txt
-pip freeze > requirements.txt
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
